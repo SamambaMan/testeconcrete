1cf7cf0a1b6ff8a92a8dcd3b628e8aa284ba6060
Felipe Gomes Vieira Ferreira <felipe.ferreira@mapptv.com.br>
2017-09-21 20:50:19 -0300

    teste de build p3 1

--- usuarios/views.py
+++ usuarios/views.py
@@ -73,6 +73,9 @@ def obter(request, guid):
 
     jwttoken = jwttoken.split(' ')[1]
 
+    print(jwttoken)
+    print(guid)
+
     usuario = DetalhesUsuario.objects.obterporguidetoken(guid, jwttoken)
 
     return Response(serializarusuario(usuario), status.HTTP_200_OK)
